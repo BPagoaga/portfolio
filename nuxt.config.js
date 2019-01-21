f608dd19c845969b319c870a2607725fa7dc680a
Bernard Pagoaga <bernard.pagoaga@webdealauto.com>
2019-01-21 17:15:40 +0100

    [UPDATE] Balises meta + favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,63 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
-const nodeExternals = require('webpack-node-externals')
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
   ** Headers of the page
   */
   head: {
-    title: 'Bernard Pagoaga, Javascript Front-End Web Developer in Lille',
+    title: "Bernard Pagoaga, Javascript & PHP Web Developer in Lille",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description },
+      {
+        name: "key-words",
+        content:
+          "web development, front-end, responsive design, Lille, javascript, html, css, php, symfony, angular, vue"
+      },
+      {
+        property: "og:title",
+        content: "Bernard Pagoaga, Javascript & PHP Web Developer in Lille"
+      },
+      {
+        property: "og:type",
+        content: "website"
+      },
+      {
+        property: "og:url",
+        content: "https://bernardpagoaga.net"
+      },
+      {
+        property: "og:image",
+        content: "https://bernardpagoaga.net/BPAGOAGA.svg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Bernard Pagoaga, Javascript & PHP Web Developer in Lille, using Symfony, Angular, Vue, ApiPlatform & Co"
+      },
+      { name: "author", content: "Bernard Pagoaga" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Material+Icons' },
-      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css', integrity: 'sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU', crossorigin: 'anonymous' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Fira+Sans'
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Material+Icons"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+        integrity:
+          "sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU",
+        crossorigin: "anonymous"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Fira+Sans"
       }
     ]
   },
@@ -28,31 +65,26 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#FFFFFF' },
+  loading: { color: "#FFFFFF" },
 
   /*
   ** Global CSS
   */
-  css: [
-    'vuetify/src/stylus/main.styl',
-    '@/assets/scss/main.scss'
-  ],
+  css: ["vuetify/src/stylus/main.styl", "@/assets/scss/main.scss"],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-    '@/plugins/vuetify'
-  ],
+  plugins: ["@/plugins/vuetify"],
 
   /*
   ** Nuxt.js modules
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://github.com/vaso2/nuxt-fontawesome
-    'nuxt-fontawesome'
+    "nuxt-fontawesome"
   ],
   /*
   ** Axios module configuration
@@ -72,19 +104,19 @@ module.exports = {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
       if (ctx.isServer) {
         config.externals = [
           nodeExternals({
             whitelist: [/^vuetify/]
           })
-        ]
+        ];
       }
     }
   }
-}
+};
